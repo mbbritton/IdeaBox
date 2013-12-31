d2b7adb76eb29a0d246c9872a2cd81ca5e4249e8
dev1 <dev1@vvh-d1.(none)>
2013-12-30 18:00:16 -0600

    Added the role and user for status meister

--- IdeaBox/grails-app/conf/BootStrap.groovy
+++ IdeaBox/grails-app/conf/BootStrap.groovy
@@ -8,13 +8,16 @@ class BootStrap {
 
 		def roleAdmin = new Role(authority: 'ROLE_ADMIN').save()
 		def roleUser = new Role(authority: 'ROLE_USER').save()
+		def roleStatusMeister = new Role(authority: 'ROLE_STATUS_MEISTER').save()
 
 		def user = new User(username: 'user', password: 'password', enabled: true).save()
 		def admin = new User(username: 'admin', password: 'password', enabled: true).save()
+		def statusMeister = new User(username: 'statusmeister', password:'password', enabled: true).save()
 
 		UserRole.create user, roleUser
 		UserRole.create admin, roleUser
 		UserRole.create admin, roleAdmin, true
+		UserRole.create statusMeister, roleStatusMeister, true
 
 	}
 	def destroy = {
